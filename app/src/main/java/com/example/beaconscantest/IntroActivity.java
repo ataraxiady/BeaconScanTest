6ae053b4ec608f49fabce3972454ce652f2e21c5
ataraxiady <ataraxiady@gmail.com>
2020-07-20 15:43:09 +0900

    Beacon Scan Test Forth Commit 1. IntroActivity is added

---
+++ app/src/main/java/com/example/beaconscantest/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.beaconscantest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
